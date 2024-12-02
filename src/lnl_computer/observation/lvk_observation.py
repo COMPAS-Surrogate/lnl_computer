748c4ebaf89dedf911ae5b4b0dc0451bb4d0246b
avi <avi.vajpeyi@gmail.com>
2024-12-02 18:00:41 +1300

    Add more data filtering for LVK (add option to load specific observing runs)

--- src/lnl_computer/observation/lvk_observation.py
+++ src/lnl_computer/observation/lvk_observation.py
@@ -6,8 +6,17 @@ from .observation import Observation
 
 class LVKObservation(Observation):
     @classmethod
-    def from_ogc4_data(cls, pastro_threshold=0.95) -> "LVKObservation":
-        data = PopulationMcZ.load(pastro_threshold=pastro_threshold)
+    def from_ogc4_data(
+        cls,
+        pastro_threshold=0.95,
+        observing_runs=["O3a", "O3b"],
+        filter_valid_mcz=True,
+    ) -> "LVKObservation":
+        data = PopulationMcZ.load(
+            pastro_threshold=pastro_threshold,
+            observing_runs=observing_runs,
+            filter_valid_mcz=filter_valid_mcz,
+        )
         w = data.weights
         # change from (n_events, z_bins, mc_bins) to (n_events, mc_bins, z_bins)
         w = np.moveaxis(w, (0, 1, 2), (0, 2, 1))
